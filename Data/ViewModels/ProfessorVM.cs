c6a8a7d045145724bec9f59a2cdd3201ce1bc882
Victor G. Ramos <69207529+Ka-Konata@users.noreply.github.com>
2023-09-25 04:28:01 -0300

    Bug Fixed
    Exception [System.OverflowException: Value was either too large or too small for an Int32.] that occurred whenever a class that inherits [Person] was instantiated

--- Data/ViewModels/ProfessorVM.cs
+++ Data/ViewModels/ProfessorVM.cs
@@ -6,8 +6,8 @@ namespace API.Data.ViewModels
     {
         [Required] public string senha { get; set; }
         [Required] public string nome { get; set; }
-        [Required][Range(1, int.MaxValue, ErrorMessage = "Value for {0} must be between {1} and {2}.")] public Int64 cpf { get; set; }
-        [Required][Range(1, int.MaxValue, ErrorMessage = "Value for {0} must be between {1} and {2}.")] public Int64 rg { get; set; }
+        [Required][Range(1, Int64.MaxValue, ErrorMessage = "Value for {0} must be between {1} and {2}.")] public Int64 cpf { get; set; }
+        [Required][Range(1, Int64.MaxValue, ErrorMessage = "Value for {0} must be between {1} and {2}.")] public Int64 rg { get; set; }
         public Int64? telefone { get; set; }
         public string? email { get; set; }
     }
